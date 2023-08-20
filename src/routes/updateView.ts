dbe7982ba012c32654dc0b476e3a4fdc5e0516f2
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-21 01:32:28 +0700

    add

--- src/routes/updateView.ts
+++ src/routes/updateView.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import UpdateView from '@/controllers/UpdateViewContronllers';
+import UpdateView from '@/controllers/updateViewContronllers';
 
 const router = express.Router();
 
