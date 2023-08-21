ed9bf58b76cb102fd1526968ed856fe7949b4fa9
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-21 10:29:40 +0700

    add

--- src/routes/discover.ts
+++ src/routes/discover.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import Discover from '../controllers/discoverController';
+import Discover from '@/controllers/discoverController';
 const router = express.Router();
 
 router.get('/:slug', Discover.get);
