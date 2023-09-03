8109847d60f25b0249e0c627ae8219fe8e7be429
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-09-03 11:42:26 +0700

    add

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -7,7 +7,14 @@ class MongoDB {
     try {
       mongoose.set('strictQuery', false);
 
-      await mongoose.connect(process.env.MONGODB_URI!, {
+      const mongo_uri: string =
+        process.env.NODE_ENV == 'production'
+          ? process.env.MONGODB_URI!
+          : 'mongodb://127.0.0.1:27017/Phimhay247_DB';
+
+      console.log(process.env.NODE_ENV);
+
+      await mongoose.connect(mongo_uri, {
         dbName: 'Phimhay247_DB',
       });
 
