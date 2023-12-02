8a5a5f28fc7c6bafa925fffb1ebf7d4944d49fd2
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-02 13:24:07 +0700

    add

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import Account from '@/controllers/accountController';
 const router = express.Router();
 
+router.post('/confirm/:type', Account.confirm);
 router.post('/change-password', Account.changePassword);
 router.post('/change-email', Account.changeEmail);
 router.post('/verify-email', Account.verifyEmail);
-router.post('/confirm/:type', Account.confirm);
 
 export default router;
