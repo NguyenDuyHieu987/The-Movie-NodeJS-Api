28a9ba6db33ad072ba364644070c7e83526b3190
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-04-21 16:15:35 +0700

    chore: update

--- src/routes/movie.route.ts
+++ src/routes/movie.route.ts
@@ -2,11 +2,16 @@ import express from 'express';
 
 import Movie from '@/controllers/movie.controller';
 import MovieSlug from '@/controllers/movieSlug.controller';
+import { authenticationHandler } from '@/middlewares';
 
 const router = express.Router();
 
 router.get('/detail/:id', Movie.get);
-router.get('/update-view/:id', Movie.updateView);
+router.post(
+  '/update-view/:id',
+  (...params) => authenticationHandler(...params, { required: true }),
+  Movie.updateView
+);
 router.get('/:slug', MovieSlug.getSlug);
 router.get('/discover/:slug', MovieSlug.filter);
 
--- src/routes/tv.route.ts
+++ src/routes/tv.route.ts
@@ -2,11 +2,16 @@ import express from 'express';
 
 import TV from '@/controllers/tv.controller';
 import TvSlug from '@/controllers/tvSlug.controller';
+import { authenticationHandler } from '@/middlewares';
 
 const router = express.Router();
 
 router.get('/detail/:id', TV.get);
-router.get('/update-view/:id', TV.updateView);
+router.post(
+  '/update-view/:id',
+  (...params) => authenticationHandler(...params, { required: true }),
+  TV.updateView
+);
 router.get('/:slug', TvSlug.getSlug);
 router.get('/discover/:slug', TvSlug.filter);
 
