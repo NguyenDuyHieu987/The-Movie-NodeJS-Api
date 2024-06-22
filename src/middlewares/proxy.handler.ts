ab68d08dda1c0d5da459939898bf4cb06c1e1e81
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-06-22 17:29:37 +0700

    chore: update

--- src/middlewares/proxy.handler.ts
+++ src/middlewares/proxy.handler.ts
@@ -4,7 +4,6 @@ const proxyHandler = (req: Request, res: Response, next: NextFunction) => {
   const proxyAuthHeader = req.headers['x-proxy-auth'];
   const apiKey = req.headers['x-api-key'];
   const requestIP = req.ip || req.connection.remoteAddress;
-  console.log(requestIP);
 
   if (
     proxyAuthHeader !== 'my-proxy-key'
