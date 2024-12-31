1e939f43cacf7d78d2bfece189b35c6db6ae7e08
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-12-31 22:12:12 +0700

    chore: update

--- src/controllers/genre.controller.ts
+++ src/controllers/genre.controller.ts
@@ -1,16 +1,20 @@
 import type { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
+import { v4 as uuidv4 } from 'uuid';
 
 import { RedisCache } from '@/config/redis';
 import Genre from '@/models/genre';
+import { GenreForm } from '@/types';
+import { DeleteResult } from 'mongoose';
 
 export class GenreController extends RedisCache {
   async getAll(req: Request, res: Response, next: NextFunction) {
     try {
+      const noCache: boolean = !!req.query?.no_cache;
       const key: string = req.originalUrl;
       const dataCache: any = await RedisCache.client.get(key);
 
-      if (dataCache != null) {
+      if (dataCache != null && !noCache) {
         return res.json(JSON.parse(dataCache));
       }
 
@@ -18,13 +22,208 @@ export class GenreController extends RedisCache {
 
       const response = { results: data };
 
-      await RedisCache.client.setEx(
-        key,
-        +process.env.REDIS_CACHE_TIME!,
-        JSON.stringify(response)
-      );
+      if (data.length > 0 && !noCache) {
+        await RedisCache.client.setEx(
+          key,
+          +process.env.REDIS_CACHE_TIME!,
+          JSON.stringify(response)
+        );
+      }
 
       return res.json(response);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async search(req: Request, res: Response, next: NextFunction) {
+    try {
+      const query: string = (req.query.query as string) || '';
+      const noCache: boolean = !!req.query?.no_cache;
+      const key: string = req.originalUrl;
+      const dataCache: any = await RedisCache.client.get(key);
+
+      if (dataCache != null && !noCache) {
+        return res.json(JSON.parse(dataCache));
+      }
+
+      const data = await Genre.find({
+        name: { $regex: query, $options: 'i' }
+      });
+
+      const response = { results: data };
+
+      if (data.length > 0 && !noCache) {
+        await RedisCache.client.setEx(
+          key,
+          +process.env.REDIS_CACHE_TIME!,
+          JSON.stringify(response)
+        );
+      }
+
+      return res.json(response);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const formData: GenreForm = req.body;
+
+      if (!formData) {
+        throw createHttpError.InternalServerError(
+          'Please provide full genre information'
+        );
+      }
+
+      const id: string = uuidv4();
+
+      const result = Genre.create({
+        id: id,
+        ...req.body,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString()
+      });
+
+      if (result == null) {
+        throw createHttpError.InternalServerError('Add genre failed');
+      }
+
+      return res.json({
+        success: true,
+        result: result
+      });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async updateGenre(req: Request, res: Response, next: NextFunction) {
+    try {
+      const formData: GenreForm = req.body;
+
+      if (!formData) {
+        throw createHttpError.InternalServerError(
+          'Please provide full genre information'
+        );
+      }
+
+      const genreId: string = req.params.id;
+
+      const result = await Genre.updateOne(
+        {
+          id: genreId
+        },
+        {
+          $set: {
+            name: formData.name,
+            name_vietsub: formData.name_vietsub,
+            short_name: formData.short_name,
+            updated_at: new Date().toISOString()
+          }
+        }
+      );
+
+      if (result.modifiedCount != 1) {
+        return next(
+          createHttpError.InternalServerError('Update video path failed')
+        );
+      }
+
+      return res.json({
+        success: true,
+        result: result
+      });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async updateVideoUpload(req: Request, res: Response, next: NextFunction) {
+    try {
+      const stillPath: string = req.body?.still_path;
+      const videoPath: string = req.body?.video_path;
+      const duration: string = req.body?.duration;
+
+      if (!videoPath) {
+        throw createHttpError.InternalServerError('Please provide video path');
+      }
+
+      const genreId: string = req.params.id;
+
+      const result = await Genre.updateOne(
+        {
+          id: genreId
+        },
+        {
+          $set: {
+            still_path: stillPath,
+            video_path: videoPath,
+            duration: duration,
+            runtime: duration,
+            updated_at: new Date().toISOString()
+          }
+        }
+      );
+
+      if (result.matchedCount != 1) {
+        return next(
+          createHttpError.InternalServerError('Update video path failed')
+        );
+      }
+
+      return res.json({
+        success: true,
+        result: result
+      });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async deleteGenre(req: Request, res: Response, next: NextFunction) {
+    try {
+      const genreId: string = req.params.id;
+
+      const result = await Genre.deleteOne({
+        id: genreId
+      });
+
+      if (result.deletedCount != 1) {
+        return next(createHttpError.InternalServerError('Delete genre failed'));
+      }
+
+      return res.json({
+        success: true,
+        message: 'Delete genre suucessfully'
+      });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async deleteGenreMultiple(req: Request, res: Response, next: NextFunction) {
+    try {
+      const listGenreId: string[] | number[] = req.body.listGenreId;
+      var results: DeleteResult[] = [];
+      for (var genreId of listGenreId) {
+        const result = await Genre.deleteOne({
+          id: genreId
+        });
+        results.push(result);
+      }
+
+      if (results.some((r) => !r.acknowledged)) {
+        return next(
+          createHttpError.InternalServerError('Delete genres failed')
+        );
+      }
+
+      return res.json({
+        success: true,
+        message: 'Delete genres suucessfully'
+      });
     } catch (error) {
       return next(error);
     }
--- src/routes/genre.route.ts
+++ src/routes/genre.route.ts
@@ -4,5 +4,30 @@ import Genre from '@/controllers/genre.controller';
 const router = express.Router();
 
 router.get('/get-all', Genre.getAll);
+router.get('/search/:movieId/:seasonId', Genre.search);
+router.post(
+  '/create',
+  (...params) =>
+    authenticationHandler(...params, { required: true, role: ['admin'] }),
+  Genre.create
+);
+router.post(
+  '/update/:id',
+  (...params) =>
+    authenticationHandler(...params, { required: true, role: ['admin'] }),
+  Genre.updateGenre
+);
+router.delete(
+  '/delete/:id',
+  (...params) =>
+    authenticationHandler(...params, { required: true, role: ['admin'] }),
+  Genre.deleteGenre
+);
+router.delete(
+  '/delete-multiple',
+  (...params) =>
+    authenticationHandler(...params, { required: true, role: ['admin'] }),
+  Genre.deleteGenreMultiple
+);
 
 export default router;
